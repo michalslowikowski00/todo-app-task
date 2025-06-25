e403689462eb69bcbb3135525331c325595d3b8b
michalslowikowski <michalslowikowski00@gmail.com>
2025-06-25 08:52:57 +0200

    delete test

--- tests/todoTests.spec.ts
+++ tests/todoTests.spec.ts
@@ -8,52 +8,57 @@ describe("User", () => {
   // todo: add beforeEach hook to navigate to homepage and click on add task button
 
   test("should be able to add a new TODO item", async ({ page }) => {
-    // const homepage = new Homepage(page);
-    // await homepage.goto();
-    // //todo: fix it in the POM
-    // homepage.addTaskButtonWithinHomepage;
-
+    // given
     const homepage = new Homepage(page);
     await homepage.goto();
     homepage.addTaskButtonWithinHomepage;
 
+    // when
     const addTodoModal = new AddTodoModal(page);
     await addTodoModal.addTask("Buy a bike");
     await addTodoModal.addTaskButtonWithinModal.click();
 
-    // const addTodoModal = new AddTodoModal(page);
-    // await addTodoModal.addTask("Buy a bike");
+    // then
     const todoItem = page.getByTestId("todo-Buy a bike");
     await expect(todoItem).toBeVisible();
   });
 
   test("should be able to mark TODO as completed", async ({ page }) => {
+    // given
     const homepage = new Homepage(page);
     await homepage.goto();
     homepage.addTaskButtonWithinHomepage;
 
+    // when
     const addTodoModal = new AddTodoModal(page);
     await addTodoModal.addTask("Buy a bike");
     await addTodoModal.addTaskButtonWithinModal.click();
 
+    // and
     const checkButton = page.getByTestId("check-button");
     checkButton.click();
+
+    // then
     const todoItem = page.getByTestId("todo-Buy a bike");
     await expect(todoItem).toHaveClass(/todoItem_todoText--completed/);
   });
 
   test("should be able delete a TODO item", async ({ page }) => {
+    // given
     const homepage = new Homepage(page);
     await homepage.goto();
     homepage.addTaskButtonWithinHomepage;
 
+    // when
     const addTodoModal = new AddTodoModal(page);
     await addTodoModal.addTask("Buy a bike");
     await addTodoModal.addTaskButtonWithinModal.click();
 
+    // and
     const deleteButton = page.getByTestId("delete-button");
     await deleteButton.click();
 
+    // then
     const todoItem = page.getByTestId("todo-Buy a bike");
     await expect(todoItem).not.toBeVisible();
   });
