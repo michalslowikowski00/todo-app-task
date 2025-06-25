4cb3d02db56c2f862190bedf8627bb70e264abd5
michalslowikowski <michalslowikowski00@gmail.com>
2025-06-25 08:48:23 +0200

    change first test -- simplified

--- tests/todoTests.spec.ts
+++ tests/todoTests.spec.ts
@@ -7,15 +7,24 @@ import { add } from "date-fns";
 describe("User", () => {
   // todo: add beforeEach hook to navigate to homepage and click on add task button
 
-  test.skip("should be able to add a new TODO item", async ({ page }) => {
+  test("should be able to add a new TODO item", async ({ page }) => {
+    // const homepage = new Homepage(page);
+    // await homepage.goto();
+    // //todo: fix it in the POM
+    // homepage.addTaskButtonWithinHomepage;
+
     const homepage = new Homepage(page);
     await homepage.goto();
-    //todo: fix it in the POM
     homepage.addTaskButtonWithinHomepage;
 
     const addTodoModal = new AddTodoModal(page);
     await addTodoModal.addTask("Buy a bike");
-    homepage.validateAddedTask("Buy a bike");
+    await addTodoModal.addTaskButtonWithinModal.click();
+
+    // const addTodoModal = new AddTodoModal(page);
+    // await addTodoModal.addTask("Buy a bike");
+    const todoItem = page.getByTestId("todo-Buy a bike");
+    await expect(todoItem).toBeVisible();
   });
 
   test("should be able to mark TODO as completed", async ({ page }) => {
@@ -29,7 +38,23 @@ describe("User", () => {
 
     const checkButton = page.getByTestId("check-button");
     checkButton.click();
-    const todoItem = page.locator("p.todoItem_todoText__j68oh");
+    const todoItem = page.getByTestId("todo-Buy a bike");
     await expect(todoItem).toHaveClass(/todoItem_todoText--completed/);
+  });
+
+  test("should be able delete a TODO item", async ({ page }) => {
+    const homepage = new Homepage(page);
+    await homepage.goto();
+    homepage.addTaskButtonWithinHomepage;
+
+    const addTodoModal = new AddTodoModal(page);
+    await addTodoModal.addTask("Buy a bike");
+    await addTodoModal.addTaskButtonWithinModal.click();
+
+    const deleteButton = page.getByTestId("delete-button");
+    await deleteButton.click();
+
+    const todoItem = page.getByTestId("todo-Buy a bike");
+    await expect(todoItem).not.toBeVisible();
   });
 });
